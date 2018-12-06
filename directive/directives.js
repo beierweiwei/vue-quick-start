d844d8f00fd504027a310061d14967ff16fbf0ad
a.wang <364704059@qq.com>
2018-12-07 00:03:13 +0800

    some util

---
+++ directive/directives.js
@@ -0,0 +1,10 @@
+import draggable from './module/draggable'
+import clipboard from './module/clipboard'
+import clickoutside from './click-outisde'
+const directives = {
+  draggable,
+  clipboard,
+  clickoutside
+}
+
+export default directives
